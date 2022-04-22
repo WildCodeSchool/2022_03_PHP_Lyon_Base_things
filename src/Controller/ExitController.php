99d4db6975e99a347b49e0821b1327d2de812b82
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-04-22 16:00:21 +0200

    progress on checkboxs add on DB, but not terminated

--- src/Controller/ExitController.php
+++ src/Controller/ExitController.php
@@ -24,9 +24,11 @@ class ExitController extends AbstractController
              // Nettoie $_POST data
             $exit = array_map('trim', $_POST);
             // Securité en php
-            // chemin vers un dossier sur le serveur qui va recevoir les fichiers uploadés (attention ce dossier doit être accessible en écriture)
+            // chemin vers un dossier sur le serveur qui va recevoir les fichiers uploadés
+            // (attention ce dossier doit être accessible en écriture)
             $uploadDir = 'assets/images/';
-            // le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client (mais d'autre stratégies de nommage sont possibles)
+            // le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client
+            // (mais d'autre stratégies de nommage sont possibles)
             $uploadFile = $uploadDir . basename($_FILES['image']['name']);
             // Récupère l'extension du fichier
             $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
@@ -35,27 +37,53 @@ class ExitController extends AbstractController
             // Poids max géré par PHP par défaut est de 2M
             $maxFileSize = 2000000;
             // Je sécurise et effectue mes tests
-            // Si l'extension est autorisée 
-            if( (!in_array($extension, $authorizedExtensions))){
-                $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
+            // Si l'extension est autorisée
+            if ((!in_array($extension, $authorizedExtensions))) {
+                echo 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
             }
             // Vérifie si l'image existe et si le poids est autorisé en octets
-            if( file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize)
-            {
-            $errors[] = "Votre fichier doit faire moins de 2M !";
+            if (file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize) {
+                echo 'Votre fichier doit faire moins de 2M !';
             }
             move_uploaded_file($_FILES['image']['tmp_name'], $uploadFile);
-            // Ajout du nom de l'image dans le tableau "exit" 
-            $exit ['image']= $uploadFile;
-            // Si la validation est OK, insert les données dans la DB
+            // Ajout du nom de l'image dans le tableau "exit"
+            $exit ['image'] = $uploadFile;
+
+
+            
+            
             $exitManager = new ExitManager();
             $id = $exitManager->insert($exit);
+
+
+/*          
+            
+            $exit['jumpTypes'] = [1,2,5];
+            echo $exit['jumpTypes'][0]; */
             // Redirige vers le détail de l'éxit que l'on vient de créer
+
             header('Location:/Exit/index?id=' . $id);
             return null;
         }
         return $this->twig->render('Exit/add.html.twig');
     }
+    
+        public function addTypeJump(): int
+        {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            
+            $exit = array_map('trim', $_POST);
+            
+            $exitManager = new ExitManager();
+            $exit['jumpTypes'] = [1,2,5];
+            $id = $exitManager->addJumpType($exit);
+
+            
+            echo $exit['jumpTypes'][0];
 
 
+            return $id;
+        }
+    }
 }
--- src/Model/ExitManager.php
+++ src/Model/ExitManager.php
@@ -16,7 +16,8 @@ class ExitManager extends AbstractManager
     public function insert(array $exits): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " 
-        (`name`, `department`, `country`, `height`, `access_duration`, `gps_coordinates`, `acces`, `remark`, `video`, `image`) 
+        (`name`, `department`, `country`, `height`, `access_duration`, `gps_coordinates`, `acces`,
+        `remark`, `video`, `image`) 
         VALUES 
         (:name, :department, :country, :height, :access_duration, :gps_coordinates, :acces, :remark, :video, :image)");
         $statement->bindValue('name', $exits['name'], \PDO::PARAM_STR);
@@ -29,9 +30,23 @@ class ExitManager extends AbstractManager
         $statement->bindValue('remark', $exits['remark'], \PDO::PARAM_STR);
         $statement->bindValue('video', $exits['video'], \PDO::PARAM_STR);
         $statement->bindValue('image', $exits['image'], \PDO::PARAM_STR);
-
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
     }
 
+    public function addJumpType($exitId, array $jumpType): int
+    {
+        $checkbox = $_POST['jumpTypes'];
+        foreach($ĵumpTypeIds as $jumpTypeId)
+        $statement = $this->pdo->prepare("INSERT INTO $exitId(jumpTypes) VALUES ('$checkbox[jumpTypes]'");
+        
+
+        return (int)$this->pdo->lastInsertId();
+    }
 }
+
+
+        /* Requette a envoyer :$statement1 = $this->pdo->prepare("INSERT INTO exit_has_type_jump (id_exit, id_type_jump) 
+        VALUES ((SELECT id FROM `exit` ORDER BY id DESC LIMIT 1),$varIdTypeJump)"); */
+
+
