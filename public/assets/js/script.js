832366baaf012d65d52472bbb7196696083f3bd2
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-05-04 11:45:53 +0200

    add condition for execute timeout code if confirmation message is present

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,9 @@
-setTimeout(function() {
-  const deleteMessage = document.getElementById('deleteMessage');
-  deleteMessage.classList.remove('alert', 'alert-success', 'd-flex', 'align-items-center');
-  deleteMessage.classList.add("hiddenMessage")
+/* Script pour faire disparaitre le message de confirmation de suppression au bout de 5 secs */
+
+if (document.getElementById('deleteMessage')) {
+  setTimeout(function () {
+    const deleteMessage = document.getElementById('deleteMessage');
+    deleteMessage.classList.remove('alert', 'alert-success', 'd-flex', 'align-items-center');
+    deleteMessage.classList.add("hiddenMessage")
   }, 5000);
-  
+}
