f4dc7ab8aed9443b6228174e871fc8c403a8233f
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-04 22:28:44 +0200

    add pseudo for filter in View page

--- src/Controller/JumpLogController.php
+++ src/Controller/JumpLogController.php
@@ -1,10 +1,12 @@
 <?php
 
 /* creation of the ExitController class to pass requests to the database */
+
 namespace App\Controller;
 
 use App\Model\JumpLogManager;
 use App\Controller\AdminController;
+use App\Service\JumpLogFilterService;
 
 class JumpLogController extends AbstractController
 {
@@ -16,7 +18,12 @@ class JumpLogController extends AbstractController
         $isLogIn = AdminController::isLogIn();
         $jumpLogManager = new JumpLogManager();
         $jumpLogs = $jumpLogManager->selectJumpExit('date_of_jump');
+        $pseudoForFilters = JumpLogFilterService::retrievePseudoForFilters($jumpLogs);
 
-        return $this->twig->render('JumpLog/index.html.twig', ['jumpLogs' => $jumpLogs, 'islogin' => $isLogIn]);
+        return $this->twig->render('JumpLog/index.html.twig', [
+            'jumpLogs' => $jumpLogs,
+            'pseudoForFilters' => $pseudoForFilters,
+            'islogin' => $isLogIn
+        ]);
     }
 }
